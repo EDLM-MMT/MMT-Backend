4b467e82f0d6aac2bfa3cfc2b7b2940c923f106b
JDTobin <90791836+JDTobin@users.noreply.github.com>
2025-03-03 13:44:52 -0500

    add CSRF_COOKIE_DOMAIN

--- app/mmt_backend_project/settings.py
+++ app/mmt_backend_project/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = ['*', 'https://*', 'https://mmt.deloitteopenlxp.com',]
 CORS_ALLOWED_ORIGINS = [
     '*', 'https://*', 'https://mmt.deloitteopenlxp.com', 'mmt.deloitteopenlxp.com']
+CSRF_COOKIE_DOMAIN = '.deloitteopenlxp.com'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
