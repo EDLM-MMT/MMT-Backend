4f9777ddba3bfad6ad81b6296afa4a1c1c652665
JDTobin <90791836+JDTobin@users.noreply.github.com>
2025-03-05 11:21:25 -0500

    add tests

--- app/generate_transcript/tests/test_setup.py
+++ app/generate_transcript/tests/test_setup.py
@@ -2,7 +2,9 @@ from rest_framework.test import APITestCase
 
 from academic_institute.models import AcademicInstitute
 from generate_transcript.models import (AcademicCourse, AcademicCourseArea,
-                                        AreasAndHour, Degree, MilitaryCourse)
+                                        AreasAndHour, Degree, MilitaryCourse,
+                                        Transcript)
+from users.models import MMTUser, UserRecord
 
 
 class TestSetUp(APITestCase):
@@ -16,6 +18,13 @@ class TestSetUp(APITestCase):
         self.c_institute = "institute1"
         self.code = "1234"
         self.hours = 10
+        self.email = "admin@example.com"
+        self.uname = "username"
+
+        self.user = MMTUser.objects.create_user(self.uname, "password")
+        self.ur = UserRecord(user_profile=self.user, email=self.email)
+
+        self.transcript = Transcript(subject=self.ur)
 
         self.ac_course_area = AcademicCourseArea(course_area=self.c_area)
         self.ac_course = \
