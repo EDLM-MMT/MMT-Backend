22961d6b406d91f612d35a8db3471040109ce9c1
JDTobin <james.d.tobin3.ctr@mail.mil>
2026-01-30 18:22:29 -0500

    update documentation
    Signed-off-by: JDTobin <james.d.tobin3.ctr@mail.mil>

--- app/mmt_backend_project/settings.py
+++ app/mmt_backend_project/settings.py
@@ -206,9 +206,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = '/opt/shared/media'
 
-if os.environ.get('FORCE_SCRIPT_NAME') is not None:
-    FORCE_SCRIPT_NAME = os.environ.get('FORCE_SCRIPT_NAME')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
